38fd96f2b25cbbc0c857be5f3f24009ccac15122
sumanapala006 <113164835+sumanapala006@users.noreply.github.com>
2022-09-09 12:15:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,9 @@ global.APIs = {
 global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
-
+F
 global.alivemsg = `📤I am alive now😇
+*ɪ ᴀᴍ ᴅᴜʟᴀᴋꜱʜᴀɴᴀ ᴡʜᴀᴛꜱᴀᴘᴘ_ʙᴏᴛ😈🍃💫*
 🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
 
 
@@ -29,34 +30,34 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
+global.vcardowner = ['+94771263209'] // owner number
+global.ownername = "ᴅᴜʟᴀᴋꜱʜᴀɴᴀ_ʙᴏᴛ😈" // owner name
+global.ytname = "" // ᴅᴜʟᴀ ᴍᴜꜱɪᴄ
+global.socialm = "you tude: https://youtube.com/channel/UC5xjWnSQA5SEAKFtFP9H8VA" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://i.ibb.co/yhCF8WP/94-77-126-3209-20220908-223428.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
-global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
+global.callblock = "true" // call block "true" / "false" ("true"  to block callers)
 global.antibad = "false" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
-global.antilink = "false" // To remove group link sharing peoples
+global.antilink = "true" // To remove group link sharing peoples
 global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94771263209']
+global.ownertag = '+941263209' //your tag number
+global.botname = 'ᴅᴜʟᴀ ʙᴏᴛ ' //ur bot name
+global.packname = "ᴅᴜʟᴀ_ʙᴏᴛ😈 Sticker" // sticker packname
+global.author = "ᴅᴜʟᴀᴋꜱʜᴀɴᴀ_😈💫" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+941263209'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
